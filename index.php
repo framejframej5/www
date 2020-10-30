bb480959173271883b1b7af95bc3ac2bd2d7483b
framejframej5 <73678211+framejframej5@users.noreply.github.com>
2020-10-30 10:01:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
 echo "<h2>PHP is Fun!<h/2>";
 echo "Hello World!<br>";
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>MY First Paragraph</p>
+        <img src="cat.jpg"
+        <br>
+        <a href="http://www.it.kmitl.ac.th">this is a Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+
+    </body>
+</html>
 ?>
